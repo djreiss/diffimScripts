0c6a8aeca9ab394a4fb176cc6daf5df9964c834c
David Reiss <reiss@uw.edu>
2017-09-19 13:05:34 -0700

    Add preconv runs

--- script2.sh
+++ script2.sh
@@ -17,6 +17,26 @@ cat output_ALDec_noSpatial.txt
 		    --config doSpatiallyVarying=True) >& output_ALDec_yesSpatial.txt
 cat output_ALDec_yesSpatial.txt
 
+(time imageDifference.py calexpDir_b1631 --output decamDirTest_ALPreconv \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimConfig.py --clobber-config --clobber-versions \
+                    --config doDecorrelation=False --config doPreConvolve=True) >& output_ALPreconv.txt
+cat output_ALPreconv.txt
+
+(time imageDifference.py calexpDir_b1631 --output decamDirTest_ALDecPreconv_noSpatial \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimConfig.py --clobber-config --clobber-versions \
+                    --config doDecorrelation=True --config doPreConvolve=True \
+		    --config doSpatiallyVarying=False) >& output_ALDecPreconv_noSpatial.txt
+cat output_ALDecPreconv_noSpatial.txt
+
+(time imageDifference.py calexpDir_b1631 --output decamDirTest_ALDecPreconv_yesSpatial \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimConfig.py --clobber-config --clobber-versions \
+                    --config doDecorrelation=True --config doPreConvolve=True \
+		    --config doSpatiallyVarying=True) >& output_ALDecPreconv_yesSpatial.txt
+cat output_ALDecPreconv_yesSpatial.txt
+
 rpl -q 'inImageSpace=True' 'inImageSpace=False' diffimConfig.py
 (time imageDifference.py calexpDir_b1631 --output decamDirTest_Zogy_noSpatial \
                     --id visit=289820 ccdnum=11 --templateId visit=288976 \
@@ -31,6 +51,22 @@ rpl -q 'inImageSpace=True' 'inImageSpace=False' diffimConfig.py
 		    --config subtract='zogy' \
 		    --config doSpatiallyVarying=True) >& output_Zogy_yesSpatial.txt
 cat output_Zogy_yesSpatial.txt
+
+rpl -q 'inImageSpace=True' 'inImageSpace=False' diffimConfig.py
+(time imageDifference.py calexpDir_b1631 --output decamDirTest_ZogyScorr_noSpatial \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimConfig.py --clobber-config --clobber-versions \
+		    --config subtract='zogy' --config doPreConvolve=True \
+		    --config doSpatiallyVarying=False) >& output_ZogyScorr_noSpatial.txt
+cat output_ZogyScorr_noSpatial.txt
+
+rpl -q 'inImageSpace=True' 'inImageSpace=False' diffimConfig.py
+(time imageDifference.py calexpDir_b1631 --output decamDirTest_ZogyScorr_yesSpatial \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimConfig.py --clobber-config --clobber-versions \
+		    --config subtract='zogy' --config doPreConvolve=True \
+		    --config doSpatiallyVarying=True) >& output_ZogyScorr_yesSpatial.txt
+cat output_ZogyScorr_yesSpatial.txt
 
 rpl -q 'inImageSpace=False' 'inImageSpace=True' diffimConfig.py
 (time imageDifference.py calexpDir_b1631 --output decamDirTest_ZogyImSpace_noSpatial \
