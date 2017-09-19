ef215168d3a349fcad6dd9291e305849b7529e6e
David Reiss <reiss@uw.edu>
2017-09-19 14:51:00 -0700

    Add post-refactor scripts

---
+++ post_refactor/diffimconfig_OLD.py
@@ -0,0 +1,11 @@
+#config.doPreConvolve=False
+#config.doMatchSources=False
+#config.doAddMetrics=False
+#config.doUseRegister=False
+#config.doSelectSources=False
+#config.kernelSourcesFromRef=False
+config.doWriteMatchedExp=True
+config.doDecorrelation=True
+
+from lsst.ip.diffim.getTemplate import GetCalexpAsTemplateTask
+config.getTemplate.retarget(GetCalexpAsTemplateTask)
